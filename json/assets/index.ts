041927632130e737f8a61342a0f2010d2326855e
Anas Alam <anasalam027@gmail.com>
2024-11-21 03:04:24 +0530

    initialisation

---
+++ json/assets/index.ts
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  logo: "/assets/images/logo.svg",
+  logo2: "/assets/images/logo2.png"
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,56 @@
+import EventListeners from "@/components/EventListener/EventListener";
+import { checkWindow } from "@/lib/functions/_helpers.lib";
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppContext, AppProps } from "next/app";
+import App from "next/app";
+import React from "react";
+import { Toaster } from "sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+/**
+ * It suppresses the useLayoutEffect warning when running in SSR mode
+ */
+function fixSSRLayout() {
+  // suppress useLayoutEffect (and its warnings) when not running in a browser
+  // hence when running in SSR mode
+  if (!checkWindow()) {
+    React.useLayoutEffect = () => {
+      // console.log("layout effect")
+    };
+  }
 }
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      // refetchOnMount: false,
+      retry: 0
+    }
+  }
+});
+
+export default function CustomApp({ Component, pageProps }: AppProps) {
+  fixSSRLayout();
+
+  return (
+    <main>
+      {/* <SessionProvider session={pageProps.session}> */}
+      <QueryClientProvider client={queryClient}>
+        <EventListeners />
+        <Toaster richColors position="bottom-left" />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+      {/* </SessionProvider> */}
+    </main>
+  );
+}
+
+CustomApp.getInitialProps = async (context: AppContext) => {
+  // // const client = initializeApollo({ headers: context.ctx.req?.headers });
+  // // resetServerContext();
+  const appProps = await App.getInitialProps(context);
+
+  return { ...appProps };
+};
